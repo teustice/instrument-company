002cb33625a6908b7869a37265b978400b300cb5
Tanner Eustice <teustice@msn.com>
2017-07-07 11:47:45 -0700

    style the instruments page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root 'landing#index'
   devise_for :users
   resources :instruments do
-    resources :reviews  
+    resources :reviews
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
