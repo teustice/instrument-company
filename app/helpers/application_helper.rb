a3a3fbdff0af016404457232407b07fd40dc7c80
Tanner Eustice <teustice@msn.com>
2017-07-07 12:45:26 -0700

    add styling to landing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def isAdmin?
+    current_user && current_user.admin
+  end
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,11 +3,12 @@ RailsAdmin.config do |config|
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
+  config.authorize_with do
+    unless current_user.try(:admin?)
+      redirect_to main_app.root_path
+    end
+  end
+  config.current_user_method(&:current_user)
   ## == Cancan ==
   # config.authorize_with :cancan
 
